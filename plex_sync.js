da695803ecc77782b6ccc1720c5a5e04845ec16e
Ryan <rstrom1763@gmail.com>
2021-09-22 17:34:37 -0400

    Update plex_sync.js
    removed repeated code mistakenly placed in previous commit.

--- plex_sync.js
+++ plex_sync.js
@@ -1,15 +1,4 @@
 const fs = require('fs');
-const express = require('express');
-const app = express();
-const port = 8081;
-app.use(express.json({ limit: '50mb' }));
-app.listen(port);
-
-datadir = './data/'
-user_data_file = datadir + 'user_data/user_data.json'
-console.log('Listening on port ' + port + '... ');
-
-app.post('/sync', (req, res) => {const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
@@ -61,42 +50,8 @@ app.post('/newuser', (req, res) => {
     });
 
 });
-// your express configuration here
+
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(port);
 console.log('Listening on port ' + port + '... ');
-
-
-    fs.writeFile(datadir + req.headers.token + '.json', JSON.stringify(req.body), (err) => err);
-    res.send("Sync Successful");
-
-});
-
-app.get('/diff', (req, res) => {
-
-    fs.readFile(datadir + req.headers.token + '.json', 'utf8', (err, data) => {
-
-        if (err) {
-            res.send(err);
-            console.log(err)
-            return;
-        }
-        else {
-            res.send(JSON.stringify(data));
-        };
-
-    });
-
-});
-
-app.post('/newuser', (req, res) => {
-
-    fs.appendFile(user_data_file, JSON.stringify(req.body), 'utf-8', function (err) {
-
-        if (err) throw err;
-        res.send('Success');
-
-    });
-
-});
