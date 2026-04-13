7a13d5b8b3ee9eaa3b13280d51b4e6a2e3398864
Ryan <48137622+rstrom1763@users.noreply.github.com>
2026-04-12 20:54:03 -0700

    Add `/dump/movies` endpoint to export compressed movies data
    Introduced a new Gin route that retrieves movies from the Plex database, compresses the data using Gzip, and returns it as a JSON response. Improved DB initialization and error handling for server startup.

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,17 +11,30 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	utils "github.com/rstrom1763/goUtils"
+	"github.com/rstrom1763/plex_comparisons/structs"
 )
 
-func StartServer() {
+func StartServer() error {
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		return
+		return fmt.Errorf("could not load .env: %s", err.Error())
 	}
 
 	port := os.Getenv("PORT")
 	protocol := os.Getenv("PROTOCOL")
+	plexDbPath := os.Getenv("PLEX_DB_PATH")
+
+	plexDB, err := initDB(plexDbPath)
+	if err != nil {
+		return fmt.Errorf("there was an error initializing the DB connection: " + err.Error())
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatal("could not close Plex database: ", err)
+		}
+	}(plexDB)
 
 	//Generate TLS keys if they do not already exist
 	if !(utils.FileExists("./cert.pem") && utils.FileExists("./private.key")) && protocol == "https" {
@@ -32,6 +47,35 @@ func StartServer() {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/dump/movies", func(c *gin.Context) {
+		movies, err := structs.GetMovies(plexDB)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		jsonData, err := json.Marshal(movies)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "could not marshal movies: " + err.Error(),
+			})
+			return
+		}
+
+		compressedData := utils.GzipData(jsonData)
+		if compressedData == nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": "could not gzip movies data",
+			})
+			return
+		}
+
+		c.Header("Content-Encoding", "gzip")
+		c.Data(http.StatusOK, "application/json", compressedData)
 	})
 
 	fmt.Printf("Listening for %v on port %v...\n", protocol, port) //Notifies that server is running on X port
@@ -48,4 +92,6 @@ func StartServer() {
 	} else {
 		log.Fatal("Something went wrong starting the Gin server")
 	}
+
+	return nil
 }
